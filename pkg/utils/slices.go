fd1c6fc00beff521b6e0b64b1487366adce9e33c
Lorech <laurisdedumets@gmail.com>
2024-12-09 10:38:25 +0200

    feat(go): Implement subslice extraction

--- pkg/utils/slices.go
+++ pkg/utils/slices.go
@@ -1,5 +1,7 @@
 package utils
 
+import "slices"
+
 // Remove an element from an integer slice by index.
 //
 // If the index is out of bounds, the original slice is returned.
@@ -9,4 +11,39 @@ func RemoveInt(slice []int, index int) []int {
 	}
 	result := append([]int{}, slice...) // Copy the slice to avoid modifying the original.
 	return append(result[:index], result[index+1:]...)
+}
+
+// Get the indexes of the first of a and its' closest occurance of b within
+// slice s.
+//
+// If either value is not found in the slice, the respective boundrary of the
+// value will match the respective end of the original slice.
+func SubsliceIndex[S ~[]E, E comparable](s S, a E, b E) (int, int) {
+	// The start is the position of a or the start of the original slice.
+	start := slices.Index(s, a)
+	if start == -1 {
+		start = 0
+	}
+
+	// The end is the end of the original slice or the closest position of b to a.
+	end := len(s)
+	for i := start + 1; i != len(s); i++ {
+		if s[i] == b {
+			// i is off-by-one because slicing uses the second value non-inclusively.
+			end = i + 1
+			break
+		}
+	}
+
+	return start, end
+}
+
+// Get a subslice from the first occurance of a to its' closest occurance of b
+// within slice s.
+//
+// If either value is not found in the slice, the respective boundrary of the
+// value will match the respective end of the original slice.
+func Subslice[S ~[]E, E comparable](s S, a E, b E) []E {
+	start, end := SubsliceIndex(s, a, b)
+	return s[start:end]
 }
--- pkg/utils/slices_test.go
+++ pkg/utils/slices_test.go
@@ -34,3 +34,82 @@ func TestRemoveLastIntByIndex(t *testing.T) {
 		t.Errorf("RemoveInt() = %v, expected %v", r, e)
 	}
 }
+
+// Tests subslice index retrieval with both values in the slice.
+func TestValidSubsliceIndex(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5}
+	e1, e2 := 1, 3
+	r1, r2 := SubsliceIndex(s, 2, 3)
+	if r1 != e1 || r2 != e2 {
+		t.Errorf("SubsliceIndex() = %v, %v, expected %v, %v", r1, r2, e1, e2)
+	}
+}
+
+// Tests subslice retrieval with both values in the slice.
+func TestValidSubslice(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5}
+	e := []int{2, 3}
+	r := Subslice(s, 2, 3)
+	if len(r) != len(e) || r[0] != e[0] || r[1] != e[1] {
+		t.Errorf("Subslice() = %v, expected %v", r, e)
+	}
+}
+
+// Tests subslice index retrieval with only the second value in the slice.
+func TestEndingSubsliceIndex(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5}
+	e1, e2 := 0, 3
+	r1, r2 := SubsliceIndex(s, 6, 3)
+	if r1 != e1 || r2 != e2 {
+		t.Errorf("SubsliceIndex() = %v, %v, expected %v, %v", r1, r2, e1, e2)
+	}
+}
+
+// Tests subslice retrieval with only the second value in the slice.
+func TestEndingSubslice(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5}
+	e := []int{1, 2, 3}
+	r := Subslice(s, 6, 3)
+	if len(r) != len(e) || r[0] != e[0] || r[1] != e[1] || r[2] != e[2] {
+		t.Errorf("Subslice() = %v, expected %v", r, e)
+	}
+}
+
+// Tests subslice index retrieval with only the first value in the slice.
+func TestStartingSubsliceIndex(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5}
+	e1, e2 := 1, 5
+	r1, r2 := SubsliceIndex(s, 2, 6)
+	if r1 != e1 || r2 != e2 {
+		t.Errorf("SubsliceIndex() = %v, %v, expected %v, %v", r1, r2, e1, e2)
+	}
+}
+
+// Tests subslice retrieval with only the first value in the slice.
+func TestStartingSubslice(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5}
+	e := []int{2, 3, 4, 5}
+	r := Subslice(s, 2, 6)
+	if len(r) != len(e) || r[0] != e[0] || r[1] != e[1] || r[2] != e[2] || r[3] != e[3] {
+		t.Errorf("Subslice() = %v, expected %v", r, e)
+	}
+}
+
+// Tests subslice index retrieval with neither value in the slice.
+func TestInvalidSubsliceIndex(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5}
+	e1, e2 := 0, 5
+	r1, r2 := SubsliceIndex(s, 6, 7)
+	if r1 != e1 || r2 != e2 {
+		t.Errorf("SubsliceIndex() = %v, %v, expected %v, %v", r1, r2, e1, e2)
+	}
+}
+
+// Tests subslice retrieval with neither value in the slice.
+func TestInvalidSubslice(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5}
+	r := Subslice(s, 6, 7)
+	if len(r) != len(s) || r[0] != s[0] || r[1] != s[1] || r[2] != s[2] || r[3] != s[3] || r[4] != s[4] {
+		t.Errorf("Subslice() = %v, expected %v", r, s)
+	}
+}

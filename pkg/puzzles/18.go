422706f93de70e93e8f16c00f7075768f2ac469d
Lorech <laurisdedumets@gmail.com>
2024-12-18 20:31:37 +0200

    solve(go): 2024 day eighteen, part two

--- pkg/puzzles/18.go
+++ pkg/puzzles/18.go
@@ -1,7 +1,9 @@
 package puzzles
 
 import (
+	"fmt"
 	"lorech/advent-of-code-2024/pkg/grid"
+	"math"
 	"slices"
 	"strconv"
 	"strings"
@@ -14,8 +16,8 @@ type node struct {
 
 // Day 18: RAM Run
 // https://adventofcode.com/2024/day/18
-func dayEighteen(input string) (int, int) {
-	return d18p1(input), 0
+func dayEighteen(input string) (interface{}, interface{}) {
+	return d18p1(input), d18p2(input)
 }
 
 // Completes the first half of the puzzle for day 18.
@@ -46,6 +48,64 @@ func d18p1(input string, options ...int) int {
 	}
 
 	return len(path)
+}
+
+// Completes the second half of the puzzle for day 18.
+//
+// As test data uses different inputs, a slice of optional integer parameters
+// can be specified with up to 2 values:
+//   - width of the maze
+//   - height of the maze
+//
+// The default of these variables match the puzzle requirements (71, 71).
+func d18p2(input string, options ...int) string {
+	w, h := 71, 71
+	if len(options) > 0 {
+		w = options[0]
+		h = options[1]
+	}
+
+	obstacles := parseObstacles(input)
+	left, right := 0, len(obstacles)-1
+	middle := int(math.Floor(float64((left + right) / 2)))
+	bounds := []int{left, middle, right}
+	var result string
+
+	for true {
+		maze := createMaze(w, h, obstacles[left:middle])
+		_, success := navigateMaze(maze)
+		mStartIndex, _ := slices.BinarySearch(bounds, middle)
+		mEndIndex := mStartIndex
+
+		// Find the middle value for the next iteration based on binary search.
+		if success {
+			mEndIndex += 1
+
+		} else {
+			mEndIndex -= 1
+		}
+		middle = int(math.Floor(float64((middle + bounds[mEndIndex]) / 2)))
+
+		// We've already seen this value before, which means we know the bound.
+		if slices.Contains(bounds, middle) {
+			maze := createMaze(w, h, obstacles[left:middle])
+			_, success := navigateMaze(maze)
+			var byte [2]int
+			if success {
+				byte = obstacles[middle]
+			} else {
+				byte = obstacles[middle+1]
+			}
+			result = fmt.Sprintf("%d,%d", byte[1], byte[0])
+			break
+		}
+
+		// Prepare the bounds for the next loop and go again.
+		bounds = append(bounds, middle)
+		slices.Sort(bounds)
+	}
+
+	return result
 }
 
 // Navigates the maze using BFS, returning the finishing node, whose parent
--- pkg/puzzles/18_test.go
+++ pkg/puzzles/18_test.go
@@ -34,3 +34,33 @@ func BenchmarkDayEighteenPartOne(b *testing.B) {
 		d18p1(input)
 	}
 }
+
+// Tests the second part of the puzzle for day 18.
+func TestDayEighteenPartTwo(t *testing.T) {
+	input, err := file.ReadTestFile(18)
+
+	if err != nil {
+		t.Errorf("Could not read test file: %v", err)
+		return
+	}
+
+	e := "6,1"
+	if r := d18p2(input, 7, 7, 12); e != r {
+		t.Errorf("d18p2() = %v, expected %v", r, e)
+	}
+}
+
+// Benchmarks the second part of the puzzle for day 18.
+func BenchmarkDayEighteenPartTwo(b *testing.B) {
+	input, err := file.ReadInfile(18)
+
+	if err != nil {
+		b.Errorf("Could not read file: %v", err)
+		return
+	}
+
+	b.ResetTimer()
+	for range b.N {
+		d18p2(input)
+	}
+}

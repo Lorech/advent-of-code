d3fb5ceaa781d5e5f16618f3c6de747572b7aadd
Lorech <laurisdedumets@gmail.com>
2024-12-20 00:17:58 +0200

    solve(go): 2024 day seventeen, part two

--- pkg/puzzles/17.go
+++ pkg/puzzles/17.go
@@ -2,8 +2,10 @@ package puzzles
 
 import (
 	"fmt"
+	"lorech/advent-of-code-2024/pkg/cslices"
 	"math"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -11,17 +13,69 @@ import (
 // Day 17: Chronospacial Computer
 // https://adventofcode.com/2024/day/17
 func daySeventeen(input string) (interface{}, interface{}) {
-	return d17p1(input), 0
+	return d17p1(input), d17p2(input)
 }
 
 // Completes the first half of the puzzle for day 17.
 func d17p1(input string) string {
 	a, b, c, program := parseProgram(input)
-	return runProgram(program, &a, &b, &c)
+	return runProgram(program, a, b, c)
+}
+
+// Completes the second half of the puzzle for day 17.
+func d17p2(input string) int {
+	_, _, _, program := parseProgram(input)
+	solutions := make([]int, 0)
+	reverseEngineer([]int{}, program, &solutions)
+	slices.Sort(solutions)
+	return solutions[0]
+}
+
+// Recursively reverse engineers the programt to find all solutions that output
+// the same code as the program itself.
+func reverseEngineer(digits []int, program []int, solutions *[]int) {
+	// End of the recursion - time to check if the program is fully the same.
+	if len(digits) == len(program) {
+		a, _ := cslices.Stoi(digits, 8)
+		r := runProgram(program, a, 0, 0)
+		if match(program, r) {
+			*solutions = append(*solutions, a)
+		}
+		return
+	}
+
+	// BFS through the possible values of the next digit.
+	e := program[len(program)-len(digits)-1:]
+	for i := range 8 {
+		d := append(append([]int(nil), digits...), i)
+		a, _ := cslices.Stoi(d, 8)
+		r := runProgram(program, a, 0, 0)
+		if match(e, r) {
+			reverseEngineer(d, program, solutions)
+		}
+	}
+}
+
+// Compares an integer slice (source code) to a string (program output) to
+// determine if the program generated itself.
+func match(program []int, result string) bool {
+	r := strings.Split(result, ",")
+
+	if len(r) != len(program) {
+		return false
+	}
+
+	for i := range r {
+		if r[i] != strconv.Itoa(program[i]) {
+			return false
+		}
+	}
+
+	return true
 }
 
 // Runs the provided program, returning the string the program would output.
-func runProgram(program []int, a *int, b *int, c *int) string {
+func runProgram(program []int, a int, b int, c int) string {
 	var o strings.Builder
 	ip := 0
 
@@ -30,29 +84,29 @@ func runProgram(program []int, a *int, b *int, c *int) string {
 
 		switch opcode {
 		case 0: // ADV
-			*a = *a / int(math.Pow(2, float64(comboOperand(literal, *a, *b, *c))))
+			a = a / int(math.Pow(2, float64(comboOperand(literal, a, b, c))))
 		case 1: // BXL
-			*b = *b ^ literal
+			b = b ^ literal
 		case 2: // BST
-			*b = comboOperand(literal, *a, *b, *c) % 8
+			b = comboOperand(literal, a, b, c) % 8
 		case 3: // JNZ
-			if *a != 0 {
+			if a != 0 {
 				ip = literal
 				continue // The pointer does not move if we jump.
 			}
 		case 4: // BXC
-			*b = *b ^ *c
+			b = b ^ c
 		case 5: // OUT
-			v := comboOperand(literal, *a, *b, *c) % 8
+			v := comboOperand(literal, a, b, c) % 8
 			if o.Len() != 0 {
 				o.WriteString(fmt.Sprintf(",%d", v))
 			} else {
 				o.WriteString(fmt.Sprintf("%d", v))
 			}
 		case 6: // BDV
-			*b = *a / int(math.Pow(2, float64(comboOperand(literal, *a, *b, *c))))
+			b = a / int(math.Pow(2, float64(comboOperand(literal, a, b, c))))
 		case 7: // CDV
-			*c = *a / int(math.Pow(2, float64(comboOperand(literal, *a, *b, *c))))
+			c = a / int(math.Pow(2, float64(comboOperand(literal, a, b, c))))
 		}
 
 		ip += 2
--- pkg/puzzles/17_test.go
+++ pkg/puzzles/17_test.go
@@ -7,7 +7,7 @@ import (
 
 // Tests the first part of the puzzle for day 17.
 func TestDaySeventeenPartOne(t *testing.T) {
-	input, err := file.ReadTestFile(17)
+	input, err := file.ReadTestFile(17, "part1")
 
 	if err != nil {
 		t.Errorf("Could not read test file: %v", err)
@@ -32,5 +32,35 @@ func BenchmarkDaySeventeenPartOne(b *testing.B) {
 	b.ResetTimer()
 	for range b.N {
 		d17p1(input)
+	}
+}
+
+// Tests the second part of the puzzle for day 17.
+func TestDaySeventeenPartTwo(t *testing.T) {
+	input, err := file.ReadTestFile(17, "part2")
+
+	if err != nil {
+		t.Errorf("Could not read test file: %v", err)
+		return
+	}
+
+	e := 117440
+	if r := d17p2(input); e != r {
+		t.Errorf("d17p2() = %v, expected %v", r, e)
+	}
+}
+
+// Benchmarks the second part of the puzzle for day 17.
+func BenchmarkDaySeventeenPartTwo(b *testing.B) {
+	input, err := file.ReadInfile(17)
+
+	if err != nil {
+		b.Errorf("Could not read file: %v", err)
+		return
+	}
+
+	b.ResetTimer()
+	for range b.N {
+		d17p2(input)
 	}
 }

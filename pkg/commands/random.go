40755dbfdfd4f556b69e6e51561819c68753127f
Lorech <laurisdedumets@gmail.com>
2025-09-27 17:09:27 +0300

    refactor(cli): Implement opening random puzzle

--- pkg/commands/random.go
+++ pkg/commands/random.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"lorech/advent-of-code/pkg/aoc"
 	"lorech/advent-of-code/pkg/cmath"
+	"lorech/advent-of-code/pkg/runners"
 
 	"github.com/spf13/cobra"
 )
@@ -12,9 +13,10 @@ func NewRandomCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "random",
 		Short: "Pick a random puzzle",
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			var randY, randD int
 
+			o, _ := cmd.Flags().GetBool("open")
 			y, _ := cmd.Flags().GetInt("year")
 			d, _ := cmd.Flags().GetInt("day")
 
@@ -30,10 +32,21 @@ func NewRandomCommand() *cobra.Command {
 				randD = d
 			}
 
-			fmt.Printf("Day %d of %d", randD, randY)
+			if o {
+				url, err := aoc.PuzzleUrl(randY, randD)
+				if err != nil {
+					return err
+				}
+				runners.OpenURL(url)
+			} else {
+				fmt.Printf("Day %d of %d", randD, randY)
+			}
+
+			return nil
 		},
 	}
 
+	cmd.Flags().BoolP("open", "o", false, "open the puzzle instead of printing it")
 	cmd.Flags().IntP("day", "d", 0, "lock in the day, picking between a random year's puzzle")
 	cmd.Flags().IntP("year", "y", 0, "lock in the year, picking between it's puzzles")
 	cmd.MarkFlagsMutuallyExclusive("day", "year")

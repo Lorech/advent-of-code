3002a481db6128070a81e2ce20cd48f7088e64d1
Lorech <laurisdedumets@gmail.com>
2025-12-01 11:15:44 +0200

    solve(go): 2025 day one, part two

--- solutions/go/pkg/aoc2025/01.go
+++ solutions/go/pkg/aoc2025/01.go
@@ -8,7 +8,7 @@ import (
 // Day 1: Secret Entrance
 // https://adventofcode.com/2025/day/1
 func dayOne(input string) (int, int) {
-	return d1p1(input), 0
+	return d1p1(input), d1p2(input)
 }
 
 // Completes the first half of the puzzle for day 1.
@@ -26,6 +26,41 @@ func d1p1(input string) int {
 		}
 
 		if position == 0 {
+			zeroes += 1
+		}
+	}
+
+	return zeroes
+}
+
+// Completes the second half of the puzzle for day 1.
+func d1p2(input string) int {
+	combination, position, zeroes := parseLock(input), 50, 0
+
+	for _, move := range combination {
+		start := position
+		loops := move / 100
+		zeroes += max(loops, -loops) // Full loops guarantee passing 0
+		move %= 100                  // Exclude the full loops from the move
+		position += move
+
+		if position < 0 || position > 99 {
+			// Boundary was crossed, so update the values based on the direction
+			if position > 99 {
+				// Overflow from 99 to 0
+				position %= 100
+				zeroes += 1
+			} else if position < 0 {
+				// Underflow from 0 to 99
+				position += 100
+				// A starting position 0 was counted on the last move
+				if start != 0 {
+					zeroes += 1
+				}
+			}
+		} else if start != 0 && position == 0 {
+			// A boundary was not crossed, so add a zero if we finished the move on it,
+			// unless if we started on it, as that was already counted on the last move.
 			zeroes += 1
 		}
 	}
--- solutions/go/pkg/aoc2025/01_test.go
+++ solutions/go/pkg/aoc2025/01_test.go
@@ -34,3 +34,33 @@ func BenchmarkDayOnePartOne(b *testing.B) {
 		d1p1(input)
 	}
 }
+
+// Tests the second part of the puzzle for day 1.
+func TestDayOnePartTwo(t *testing.T) {
+	input, err := file.ReadTestFile(2025, 1)
+
+	if err != nil {
+		t.Errorf("Could not read test file: %v", err)
+		return
+	}
+
+	e := 6
+	if r := d1p2(input); e != r {
+		t.Errorf("d1p2() = %v, expected %v", r, e)
+	}
+}
+
+// Benchmarks the second part of the puzzle for day 1.
+func BenchmarkDayOnePartTwo(b *testing.B) {
+	input, err := file.ReadInfile(2025, 1)
+
+	if err != nil {
+		b.Errorf("Could not read file: %v", err)
+		return
+	}
+
+	b.ResetTimer()
+	for range b.N {
+		d1p2(input)
+	}
+}

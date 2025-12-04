99c0de297eb2910e7c1c96073b346dcfeca35769
Lorech <laurisdedumets@gmail.com>
2025-12-04 10:27:15 +0200

    solve(go): 2025 day four, part two
    The same quick and lazy solution because it was good enough

--- solutions/go/pkg/aoc2025/04.go
+++ solutions/go/pkg/aoc2025/04.go
@@ -1,11 +1,14 @@
 package aoc2025
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 // Day 4: Printing Department
 // https://adventofcode.com/2025/day/4
 func dayFour(input string) (int, int) {
-	return d4p1(input), 0
+	return d4p1(input), d4p2(input)
 }
 
 // Completes the first half of the puzzle for day 4.
@@ -45,6 +48,58 @@ func d4p1(input string) int {
 
 			if adjacent < 4 {
 				accessible += 1
+			}
+		}
+	}
+
+	return accessible
+}
+
+// Completes the second half of the puzzle for day 4.
+func d4p2(input string) int {
+	grid, accessible, removed := strings.Split(input, "\n"), 0, make([][]int, 0)
+
+	for accessible == 0 || len(removed) > 0 {
+		for _, tile := range removed {
+			grid[tile[0]] = fmt.Sprintf("%s%s%s", grid[tile[0]][:tile[1]], ".", grid[tile[0]][tile[1]+1:])
+		}
+		removed = [][]int{}
+
+		for y, row := range grid {
+			for x, tile := range row {
+				if tile != '@' {
+					continue
+				}
+
+				adjacent := 0
+				for yn := -1; yn <= 1; yn++ {
+					for xn := -1; xn <= 1; xn++ {
+						if yn == 0 && xn == 0 {
+							continue
+						}
+
+						if y+yn < 0 || y+yn >= len(grid) || x+xn < 0 || x+xn >= len(row) {
+							continue
+						}
+
+						if grid[y+yn][x+xn] == '@' {
+							adjacent += 1
+						}
+
+						if adjacent == 4 {
+							break
+						}
+					}
+
+					if adjacent == 4 {
+						break
+					}
+				}
+
+				if adjacent < 4 {
+					accessible += 1
+					removed = append(removed, []int{y, x})
+				}
 			}
 		}
 	}
--- solutions/go/pkg/aoc2025/04_test.go
+++ solutions/go/pkg/aoc2025/04_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-// Tests the second part of the puzzle for day 4.
+// Tests the first part of the puzzle for day 4.
 func TestDayFourPartOne(t *testing.T) {
 	input, err := file.ReadTestFile(2025, 4)
 
@@ -32,5 +32,35 @@ func BenchmarkDayFourPartOne(b *testing.B) {
 	b.ResetTimer()
 	for range b.N {
 		d4p1(input)
+	}
+}
+
+// Tests the second part of the puzzle for day 4.
+func TestDayFourPartTwo(t *testing.T) {
+	input, err := file.ReadTestFile(2025, 4)
+
+	if err != nil {
+		t.Errorf("Could not read test file: %v", err)
+		return
+	}
+
+	e := 43
+	if r := d4p2(input); e != r {
+		t.Errorf("d4p2() = %v, expected %v", r, e)
+	}
+}
+
+// Benchmarks the second part of the puzzle for day 4.
+func BenchmarkDayFourPartTwo(b *testing.B) {
+	input, err := file.ReadInfile(2025, 4)
+
+	if err != nil {
+		b.Errorf("Could not read file: %v", err)
+		return
+	}
+
+	b.ResetTimer()
+	for range b.N {
+		d4p2(input)
 	}
 }
